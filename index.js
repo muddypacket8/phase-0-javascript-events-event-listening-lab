1578553a9111a77124b5d67b25272f24e022aa47
collins <collinsleonkimani@outlook.com>
2023-03-26 04:10:53 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
 }
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+const button = document.querySelector('#myButton');
+button.addEventListener('click', function() {
+  console.log('Button clicked!');
+});
